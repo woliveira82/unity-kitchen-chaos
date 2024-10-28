252af9dbbba044d3a6bd15ac95f1d870768cfe38
Weiner Oliveira <woliveira82@gmail.com>
2024-10-28 11:14:09 +0000

    9 - Collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,7 +11,37 @@ public class Player : MonoBehaviour {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if (!canMove) {
+            // Attempt only X movement
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove) {
+                // Can move only on the X axis
+                moveDirection = moveDirectionX;
+
+            } else {
+                // Attempt only Z movement
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+
+                if (canMove) {
+                    // Can move only on the Z axis
+                   moveDirection = moveDirectionZ;
+                }   
+            }
+        }
+
+        if (canMove) {
+            transform.position += moveDirection * moveDistance;
+        }
 
         isWalking = moveDirection != Vector3.zero;
 
