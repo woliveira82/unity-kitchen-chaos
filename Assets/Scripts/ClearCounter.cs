efe36f9732b7b5617f85d8981cdca09359423325
Weiner Oliveira <woliveira82@gmail.com>
2024-10-30 17:14:49 +0000

    17 - Player pick up, drop objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -5,6 +5,14 @@ public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
-
+        if (HasKitchenObject()) {
+            if (!player.HasKitchenObject()) {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        } else {
+            if (player.HasKitchenObject()) {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        }
     }
 }
