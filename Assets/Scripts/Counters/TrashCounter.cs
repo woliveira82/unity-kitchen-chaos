bedfe0fd79255a07878f2bfddc03c7b19bc059e9
Weiner Oliveira <woliveira82@gmail.com>
2024-11-01 17:41:36 +0000

    22 - Trash counter

---
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,9 @@
+
+public class TrashCounter : BaseCounter {
+    
+    public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+}
