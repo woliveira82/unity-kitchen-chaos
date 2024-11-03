a59222c6d4463b522f28c8937b4ced2de98f8367
Weiner Oliveira <woliveira82@gmail.com>
2024-11-03 20:59:30 +0000

    27 - Plate world UI icons

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -32,4 +32,8 @@ public class PlateKitchenObject : KitchenObject {
         });
         return true;
     }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList() {
+        return kitchenObjectSOList;
+    }
 }
