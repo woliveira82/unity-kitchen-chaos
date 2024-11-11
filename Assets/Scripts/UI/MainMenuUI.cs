e7c97e8026f508ebd3df27fcbe4e2bc7370b5876
Weiner Oliveira <woliveira82@gmail.com>
2024-11-11 14:45:19 +0000

    35 - Main menu, loading

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour {
+
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake() {
+        playButton.onClick.AddListener(() => {
+            Loader.Load(Loader.Scene.GameScene);
+        });
+
+        playButton.onClick.AddListener(() => {
+            Application.Quit();
+        });
+    }
+}
